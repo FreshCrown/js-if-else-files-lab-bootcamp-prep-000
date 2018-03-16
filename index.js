445871b4e01f87543f369865725f3783371ad7ae
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-16 19:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var name1 = "Susan"
-var height = "74"
+var name1 = "Susan";
+var height = "74";
 
 if (name1 === "Susan") {
-    console.log("The name is correct")
+    console.log("The name is correct");
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the name to be Susan");
 }
 // Don't worry about this
 module.exports = { name1, height
